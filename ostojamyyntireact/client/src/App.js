34b7b9c9040f6237283444b07c76d5a432076bc6
Tomi <tjaariveria@gmail.com>
2021-09-15 11:03:42 +0000

    React version added

---
+++ ostojamyyntireact/client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CRUD APP</h1>
+      <div className="form">
+        <input type="text" name="movieName" />
+        <input type="text" name="review" />
+      </div>
+    </div>
+  );
+}
+
+export default App;

0e61616ac1e8a33b87298c839fda14dd4a34a2f6
tjaariveria <tjaariveria@gmail.com>
2021-09-16 10:55:26 +0300

    Refactored server connection

--- ostojamyyntireact/server/server.js
+++ ostojamyyntireact/server/server.js
@@ -25,19 +25,17 @@
 // 	console.log("Connected to database");
 //   }
 // }
-const mariadb = require('mariadb/callback');
+const mariadb = require("mariadb/callback");
 const conn = mariadb.createConnection({
-      host: 'localhost', 
-      user:'root',
-      password: 'koira123',
-	  database: 'myTable'
-    });
-conn.connect(err => {
+  host: "ec2-13-51-197-128.eu-north-1.compute.amazonaws.com",
+  user: "root",
+  password: "my-new-password",
+  database: "ostojamyynti",
+});
+conn.connect((err) => {
   if (err) {
     console.log("not connected due to error: " + err);
   } else {
-	const res = conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-	console.log(res);
     console.log("connected ! connection id is " + conn.threadId);
   }
 });

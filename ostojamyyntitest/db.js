0aa2bf7baf3d6d1625058dacf35f68c7fac6e4fe
tjaariveria <tjaariveria@gmail.com>
2021-09-20 11:30:09 +0300

    working post with no dublicates

--- ostojamyyntitest/db.js
+++ ostojamyyntitest/db.js
@@ -4,10 +4,10 @@ var mariadb = require('mariadb');
 // Create a connection pool
 var pool = 
   mariadb.createPool({
-    host: "ec2-13-51-197-128.eu-north-1.compute.amazonaws.com", 
+    host: "ec2-13-48-195-181.eu-north-1.compute.amazonaws.com", 
     user: "root", 
     password: "my-new-password",
-    database: "todo"
+    database: "ostojamyynti"
   });
  
 // Expose a method to establish connection with MariaDB SkySQL
--- ostojamyyntitest/server.js
+++ ostojamyyntitest/server.js
@@ -5,46 +5,86 @@ const port = 8080
 //jeb
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
- 
+
+
+
+// KAYTTAJAT
+
 // GET
-app.get('/tasks', async (req, res) => {
+app.get('/kayttajat', async (req, res) => {
     try {
-        const result = await db.pool.query("select * from tasks");
+        const result = await db.pool.query("select * from kayttajat");
         res.send(result);
     } catch (err) {
         throw err;
     }
 });
- 
-// POST
-app.post('/tasks', async (req, res) => {
-    let task = req.body;
+
+ // POST COPY
+// app.post('/kayttajat', async (req, res) => {
+//     let kayttaja = req.body;
+//     try {
+//         const result = await db.pool.query("insert into kayttajat set kayttaja_taso = ?, kayttaja_tunnus = ?, kayttaja_salasana = ?, kayttaja_sahkoposti = ?", [kayttaja.kayttaja_taso, kayttaja.kayttaja_tunnus, kayttaja.kayttaja_salasana, kayttaja.kayttaja_sahkoposti]);
+//         res.send(result);
+//     } catch (err) {
+//         throw err;
+//     }
+// });
+
+// POST Toimiva duplikaatin estävä
+app.post('/kayttajat', async (req, res) => {
+    let kayttaja = req.body;
     try {
-        const result = await db.pool.query("insert into tasks (description) values (?)", [task.description]);
+        const result = await db.pool.query("insert ignore into kayttajat set kayttaja_taso = ?, kayttaja_tunnus = ?, kayttaja_salasana = ?, kayttaja_sahkoposti = ?" ,[kayttaja.kayttaja_taso, kayttaja.kayttaja_tunnus, kayttaja.kayttaja_salasana, kayttaja.kayttaja_sahkoposti]);
         res.send(result);
     } catch (err) {
         throw err;
     }
 });
- 
-app.put('/tasks', async (req, res) => {
-    let task = req.body;
+
+
+app.put('/kayttajat', async (req, res) => {
+    let kayttaja = req.body;
     try {
-        const result = await db.pool.query("update tasks set description = ?, completed = ? where id = ?", [task.description, task.completed, task.id]);
+        const result = await db.pool.query("update kayttajat set description = ?, completed = ? where id = ?", [task.description, task.completed, task.id]);
         res.send(result);
     } catch (err) {
         throw err;
     } 
 });
  
-app.delete('/tasks', async (req, res) => {
+app.delete('/kayttajat', async (req, res) => {
     let id = req.query.id;
     try {
-        const result = await db.pool.query("delete from tasks where id = ?", [id]);
+        const result = await db.pool.query("delete from kayttajat where id = ?", [id]);
         res.send(result);
     } catch (err) {
         throw err;
     } 
 });
+
+// ILMOITUKSET
+
+// GET
+app.get('/ilmoitukset', async (req, res) => {
+    try {
+        const result = await db.pool.query("select * from ilmoitukset");
+        res.send(result);
+    } catch (err) {
+        throw err;
+    }
+});
+
+// POST
+app.post('/ilmoitukset', async (req, res) => {
+    let ilmoitus = req.body;
+    try {
+        const result = await db.pool.query("insert into ilmoitukset set ilmoitus_laji = ?, ilmoitus_nimi = ?, ilmoitus_kuvaus = ?, ilmoitus_paivays = ?, ilmoittaja_id = ?", [ilmoitus.ilmoitus_laji, ilmoitus.ilmoitus_nimi, ilmoitus.ilmoitus_kuvaus, ilmoitus.ilmoitus_paivays, ilmoitus.ilmoittaja_id]);
+        res.send(result);
+    } catch (err) {
+        throw err;
+    }
+});
+ 
  
 app.listen(port, () => console.log(`Listening on port ${port}`));

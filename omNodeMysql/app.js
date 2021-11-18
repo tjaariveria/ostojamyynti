ce81af229e967f06dd6735af3f9bb4352d84c02c
Tomi <tjaariveria@gmail.com>
2021-11-18 10:20:46 +0200

    Database connection

--- omNodeMysql/app.js
+++ omNodeMysql/app.js
@@ -1,11 +1,26 @@
-const express = require("express");
+const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 
-app.get("/", (req, res) => {
+const db = mysql.createConnection({
+    host: 'ec2-13-48-195-46.eu-north-1.compute.amazonaws.com',
+    user: 'root',
+    password: 'my-new-password',
+    database: 'ostojamyynti'
+});
+
+app.get('/', (req, res) => {
     res.send("<h1>Home page</h1>")
 });
 
 app.listen(3030, () => {
-    console.log("Server started on port 3030")
+    console.log("Server listening port 3030");
+    db.connect( (error) => {
+        if(error) {
+            console.log(error);
+        } else {
+            console.log("Connected to database");
+        }
+    });
 });

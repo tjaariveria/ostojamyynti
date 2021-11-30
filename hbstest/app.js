ae95d4dda36c872bf02d31119fb4ea23bb1e3a22
Tomi <tjaariveria@gmail.com>
2021-11-30 10:34:51 +0200

    Finished material

--- hbstest/app.js
+++ hbstest/app.js
@@ -1,14 +1,54 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
+import express, { urlencoded, json } from 'express';
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+import { create } from 'express-handlebars';
+import cookieParser from 'cookie-parser';
+import pages from './routes/page.routes.js';
+import register from './routes/register.routes.js';
+import auth from './routes/auth.routes.js';
+import hbsHelpers from './helpers/handlebars-helpers.js';
+
+dotenv.config();
 
 const app = express();
 
-app.engine('hbs', engine());
-app.set('view engine', 'hbs');
-app.set("views", "./views");
-
-app.get('/', (req, res) => {
-    res.render('home');
+const hbs = create({
+    extname: '.hbs',
+    helpers: hbsHelpers
 });
 
-app.listen(3000);
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+app.use(express.static('public'));
+
+app.engine('hbs', hbs.engine);
+
+app.set('view engine', 'hbs');
+
+app.use(urlencoded({ extended: false }));
+
+app.use(json());
+
+app.use(cookieParser());
+
+app.use('/', pages);
+app.use('/register', register);
+app.use('/auth', auth);
+
+app.listen(3030, () => {
+    console.log("Server started on port 3030");
+    db.connect( (error) => {
+        if(error) {
+            console.log(error);
+        } else {
+            console.log("Connected to database");
+        }
+    });
+});
+
+export default db;

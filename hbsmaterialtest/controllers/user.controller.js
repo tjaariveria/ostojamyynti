b35ca426d028bba92282fd3f56b35a80971603d8
Tomi <tjaariveria@gmail.com>
2021-11-30 15:24:23 +0200

    Error

---
+++ hbsmaterialtest/controllers/user.controller.js
@@ -0,0 +1,57 @@
+import db from '../app.js';
+import bcrypt from 'bcryptjs';
+
+const registerUser = (req, res) => {
+    // const kayttaja_tunnus = req.body.kayttaja_tunnus;
+    // const kayttaja_sahkoposti = req.body.kayttaja_sahkoposti;
+    // const kayttaja_salasana = req.body.kayttaja_salasana;
+    // const kayttaja_salasana_varmistus = req.body.kayttaja_salasana_varmistus;
+
+    const {
+        kayttaja_tunnus,
+        kayttaja_sahkoposti,
+        kayttaja_salasana,
+        kayttaja_salasana_varmistus,
+    } = req.body;
+
+    db.query(
+        "SELECT kayttaja_sahkoposti FROM kayttajat WHERE kayttaja_sahkoposti = ?",
+        [kayttaja_sahkoposti],
+        async (error, results) => {
+            if (error) {
+                console.log("Error in query: " + error);
+            }
+            if (results.length > 0) {
+                return res.render("register", {
+                    message: "Sähköposti on jo käytössä!",
+                });
+            } else if (kayttaja_salasana !== kayttaja_salasana_varmistus) {
+                return res.render("register", {
+                    message: "Salasanat eivät täsmänneet!",
+                });
+            }
+
+            let hashedPassword = await bcrypt.hash(kayttaja_salasana, 8);
+
+            db.query(
+                "INSERT INTO kayttajat SET ?",
+                {
+                    kayttaja_tunnus: kayttaja_tunnus,
+                    kayttaja_sahkoposti: kayttaja_sahkoposti,
+                    kayttaja_salasana: hashedPassword
+                },
+                (error, results) => {
+                    if (error) {
+                        console.log("Error in query: " + error);
+                    } else {
+                        return res.status(200).render('register', {
+                            message: "Rekisteröityminen onnistui!",
+                        });
+                    }
+                }
+            );
+        }
+    );
+}
+
+export default registerUser;
---
+++ hbsmaterialtest/routes/page.routes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { isLoggedIn } from '../controllers/auth.controller.js';
+
+const routes = express.Router();
+
+routes.get('/', isLoggedIn, (req, res) => {
+    res.render('index', {
+        user: req.user
+    });
+});
+
+routes.get('/register', (req, res) => {
+    res.render('register');
+});
+
+routes.get('/login', (req, res) => {
+    res.render('login');
+});
+
+export default  routes;
